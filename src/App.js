b407986a1e48fb40caffe377ffec4b46df0a4397
khyatisoni3005 <khyati.soni4035@gmail.com>
2024-05-16 21:15:28 +0530

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useMemo } from "react";
 import MovieList from "./components/MovieList";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <>
+      <Navbar />
       <MovieList />
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+
+
+            <div className="row" style={{ backgroundColor: "#08088b", color: "white", padding: "10px" }}>
+                <div className="col-2">
+                    <h3>MOVIE APP</h3>
+                </div>
+                <div className="col-8"></div>
+                <div className="col-2"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
